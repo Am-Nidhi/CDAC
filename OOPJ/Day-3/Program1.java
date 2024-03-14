207131d442cb784af756ecea1622bc968b50b194
Nidhi Chourasiya <61491132+Am-Nidhi@users.noreply.github.com>
2024-03-14 23:21:23 +0530

    Add files via upload

---
+++ OOPJ/Day-3/Program1.java
@@ -0,0 +1,9 @@
+/*) Write a program that demonstrates widening conversion from int to double and prints the result. */
+class Program1{
+    public static void main(String[] args){
+        int num1 = 10;
+        double num2 = (double)num1;
+
+        System.out.println(num2);
+    }
+}
