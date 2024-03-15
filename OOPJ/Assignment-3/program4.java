1b36cd0adc4e4777c71b408f4e49317041795d93
Nidhi Chourasiya <61491132+Am-Nidhi@users.noreply.github.com>
2024-03-15 23:55:34 +0530

    program4.java

--- OOPJ/Assignment-3/program4.java
+++ OOPJ/Assignment-3/program4.java
@@ -7,9 +7,11 @@ class Program4{
         int num1 = 10;
         float num2 = 1.1f;
         double num3 = 20.55;
+        String str = String.valueOf(num1);
 
         int res = (int)(num1+num2+num3);
-
+        
         System.out.println(res);
+        System.out.println(str);
     }
 }
